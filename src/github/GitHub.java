1c1518d2ee24f7364df9bd6050e74c137db28747
aw28@students.ptcollege.edu <>
2020-02-05 22:35:49 -0500

    Wiener

---
+++ src/github/GitHub.java
@@ -0,0 +1,15 @@
+
+package github;
+
+public class GitHub {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Watkins();
+    }
+    
+    public static void Watkins(){
+        System.out.println("Avery Watkins!!");
+    }
+    
+}
